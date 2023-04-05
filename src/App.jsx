6bf58496e142fb2b320b2f1e1852383caee89052
ÇETİN <cetinnnarasss@gmail.com>
2023-04-05 04:04:05 +0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-
+import Header from "./components/Header"
+import Products from "./components/Products"
 
 function App() {
-
-
   return (
-    <div className="App">
-      hello
+    <div className="container mx-auto p-4">
+     <Header/>
+     <Products/>
     </div>
   )
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+import ProductItem from "./ProductItem"
+
+const Products = () => {
+  return (
+    <div>
+      <ProductItem />
+    </div>
+  )
+}
+
+export default Products
