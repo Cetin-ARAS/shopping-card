c3c3a9c31afc5c6527f8c3a01e39f3211afcff85
ÇETİN <cetinnnarasss@gmail.com>
2023-04-10 23:59:11 +0300

    item

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,8 @@
 
 const ProductItem = () => {
   return (
-    <div className="border p-4 m-2 rounded-lg shadow-lg"  >
-
+    <div className="border p-4 m-2 rounded-lg shadow-lg">
+      <img className="w-full h-48 object-cover rounded-t-lg" src="https://via.placeholder.com/250" alt="product image" />
     </div>
   )
 }
